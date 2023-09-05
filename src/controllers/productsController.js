fcfc3ac9cc9f2eeb724b20494450adece15930a1
HiTzieowo <tomasvac02@gmail.com>
2023-09-05 09:52:13 -0300

    crud pt 1

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -2,6 +2,8 @@
 const fs= require("fs")
 const path = require("path");
 
+const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+
 // Creamos el objeto literal con los métodos a exportar
 const productsController = {
 
@@ -20,7 +22,34 @@ const productsController = {
     },
 
 }
+processCreate: (req, res) => {
+    // Do the magic
+    const data = req.body;
 
+    // Leer el archivo JSON y dejarlo en una variable (array)
+    const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+    // Crear un nuevo objeto literal con los datos ingresados por el usuario
+    const newProduct = {
+        // Tomar el último elemento del array, ver su ID, y sumarle 1
+        id: products[products.length - 1].id + 1,
+        name: data.name,
+        price: parseInt(data.price),
+        discount: parseInt(data.discount),
+        category: data.category,
+        description: data.description,
+        image: req.file ? req.file.filename : "default-image.png"
+    }
+ 
+		// Agregar ese objeto al array
+		products.push(newProduct);
+
+		// Volver a escribir sobre el archivo JSON
+		fs.writeFileSync(productsFilePath, JSON.stringify(products, null, " "))
+
+		// Devolverle alguna vista al usuario
+		res.redirect("/products");
+	},
 
 // Exportamos el objeto literal con los distintos métodos, que se usará en el enrutador por defecto
 module.exports = productsController;
