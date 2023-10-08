63a4c12cc5dac94c0995953199f93fd990cbb17a
HiTzieowo <tomasvac02@gmail.com>
2023-10-08 19:45:02 -0300

    dependencias

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const express = require("express");
 const path = require("path")
 const app = express();
 
+const bcrypt = require('bcryptjs')
+
     // Importamos los distintos enrutadores
     const mainRouter = require("./routers/mainRouter.js")
 
